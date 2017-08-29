af3bba23359213f74e7c38148a4f5c350ec94d8e
Siamend Darwesh <SiamendDarwesh@gmail.com>
2017-08-29 19:28:17 +0200

    Update EditProfile.js and split it into components

---
+++ modules/EditProfile/SkillLabel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function SkillLabel(props) {
+  const { label } = props;
+  return <div className="skill-label">{label}</div>;
+}
+SkillLabel.propTypes = {
+  label: PropTypes.string,
+};
+SkillLabel.defaultProps = {
+  label: '',
+};
