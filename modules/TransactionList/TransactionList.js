ba898988afced21f3a156e14502cab2c8a212f75
Rory MacQueen <rorymacqueen@gmail.com>
2017-08-31 19:07:26 +0200

    Add message when transactions list is empty
    Rather than just showing blank page, which can be
    confusing for user.

--- modules/TransactionList/TransactionList.js
+++ modules/TransactionList/TransactionList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TransitionMotion, spring } from 'react-motion';
+import { Link } from 'react-router';
 
 import Transaction from '../Transaction/Transaction';
 import FilterBar from '../FilterBar/FilterBar';
@@ -29,25 +30,44 @@ class TransactionList extends React.Component {
     );
   }
 
+  renderTransactions() {
+    const { useAnimation, transactions } = this.props;
+    return (
+      <TransitionMotion
+        willLeave={() => (useAnimation ? { transform: spring(100) } : null)}
+        styles={transactions.map(transaction => ({
+          key: transaction._id,
+          style: { transform: 0 },
+          data: transaction
+        }))}
+      >
+        {interpolatedStyles =>
+          (<div style={{ overflow: 'hidden' }}>
+            {interpolatedStyles.map(config => this.renderInterpolatedStyles(config))}
+          </div>)
+        }
+      </TransitionMotion>
+    );
+  }
+
+  renderEmptyPage() {
+    return (
+      <div className="empty-transaction-list">
+        <p>Looks like there's nothing here.</p>
+        <p>Visit the <Link to={'/'}>Community page</Link> to find new users and set up exchanges</p>
+      </div>
+    );
+  }
+
   render() {
+    const { isFetching, transactions } = this.props;
     return (
       <div className="responsive-margins">
         <FilterBar />
-        {(this.props.isFetching && <Loader />)}
-        <TransitionMotion
-          willLeave={() => (this.props.useAnimation ? { transform: spring(100) } : null)}
-          styles={this.props.transactions.map(transaction => ({
-            key: transaction._id,
-            style: { transform: 0 },
-            data: transaction
-          }))}
-        >
-          {interpolatedStyles =>
-            (<div style={{ overflow: 'hidden' }}>
-              {interpolatedStyles.map(config => this.renderInterpolatedStyles(config))}
-            </div>)
-          }
-        </TransitionMotion>
+        {(isFetching && <Loader />)}
+        {
+          transactions.length > 0 ? this.renderTransactions() : this.renderEmptyPage()
+        }
       </div>
     );
   }
