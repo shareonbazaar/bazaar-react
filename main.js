89547699d2cb0844136d9f7ed8cce1941f724a34
Rory MacQueen <rorymacqueen@gmail.com>
2017-10-16 00:18:42 -0700

    lint main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-import './public/css/style.css';
 
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, browserHistory } from 'react-router'
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 
-import { persistStore, autoRehydrate } from 'redux-persist'
-
-import routes from './modules/routes'
-import reducers from './utils/reducers'
-
+import { persistStore, autoRehydrate } from 'redux-persist';
 import { IntlProvider, addLocaleData } from 'react-intl';
+
 import en from 'react-intl/locale-data/en';
 import de from 'react-intl/locale-data/de';
 import ar from 'react-intl/locale-data/ar';
 import localeData from './locales/data.json';
+
+import routes from './modules/routes';
+import reducers from './utils/reducers';
+import './public/css/style.css';
 
 addLocaleData([...en, ...de, ...ar]);
 
@@ -35,38 +35,37 @@ const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
 const messages = localeData[languageWithoutRegionCode] || localeData[language] || localeData.en;
 
 const store = createStore(
-    reducers,
-    {},
-    compose(
-        applyMiddleware(thunkMiddleware, routerMiddleware(browserHistory)),
-        autoRehydrate()
-    )
-)
+  reducers,
+  {},
+  compose(
+    applyMiddleware(thunkMiddleware, routerMiddleware(browserHistory)),
+    autoRehydrate()
+  )
+);
 
 if (module.hot) {
   module.hot.accept();
 }
 
 class Root extends React.Component {
-    render () {
-        const { store, history } = this.props;
-        return (
-            <IntlProvider locale={language} messages={messages}>
-                <Provider store={store}>
-                    <Router routes={routes(store)} history={history}>
-                    </Router>
-                </Provider>
-            </IntlProvider>
-        )
-    }
+  render() {
+    const { store, history } = this.props;
+    return (
+      <IntlProvider locale={language} messages={messages}>
+        <Provider store={store}>
+          <Router routes={routes(store)} history={history} />
+        </Provider>
+      </IntlProvider>
+    );
+  }
 }
 
 // Delay render until state has loaded from localStorage
-persistStore(store, {blacklist: ['routing']}, () => {
-    const history = syncHistoryWithStore(browserHistory, store);
+persistStore(store, { blacklist: ['routing'] }, () => {
+  const history = syncHistoryWithStore(browserHistory, store);
 
-    render((
-        <Root store={store} history={history} />
-        ), document.getElementById('root'));
-})
+  render((
+    <Root store={store} history={history} />
+  ), document.getElementById('root'));
+});
 
